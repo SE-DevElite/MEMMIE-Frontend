389ab6ed4cd2aa8a58f824c530a8ff9cbf041ee0
nutthanon <nutthanon.tho@gmail.com>
2024-01-06 01:58:52 +0700

    feat: bottom bar

--- src/assets/svg/Plus.tsx
+++ src/assets/svg/Plus.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react'
 import Svg, { Path } from 'react-native-svg'
 
-const PlusIcon = () => {
+interface PlusIconProps {
+  color?: string
+}
+
+const PlusIcon: React.FC<PlusIconProps> = props => {
+  const { color } = props
+
   return (
     <Svg width={19} height={19} viewBox="0 0 19 19" fill="none">
       <Path
         d="M1 9.095h8.095m0 0h8.095m-8.095 0V1m0 8.095v8.095"
-        stroke="#A56073"
+        stroke={color || '#A56073'}
         strokeWidth={2}
         strokeLinecap="round"
         strokeLinejoin="round"

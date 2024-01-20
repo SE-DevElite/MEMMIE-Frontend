c744a4f80f0e5c0864fe383b0b74ace90d7aebef
nutthanon <nutthanon.tho@gmail.com>
2024-01-20 22:26:42 +0700

    feat: user profile bottom sheet

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import ButtonBackCommon from '@/common/ButtonBack.common'
 import { View, StyleSheet, ScrollView } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import UserProfileName from '@/components/profile/UserProfileName'
 import UserBio from '@/components/profile/UserBio'
 import MemoryGroup from '@/components/profile/MemoryGroup'
+import ProfileBottomSheet from '@/components/profile/ProfileBottomSheet'
+import BottomSheet from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheet/BottomSheet'
 
 const ProfileScreen: React.FC = () => {
+  const editProfileBottomSheetRef = useRef<BottomSheet>(null)
+
   return (
     <SafeAreaView edges={['right', 'top']}>
       <View style={styles.layout}>
@@ -14,10 +18,15 @@ const ProfileScreen: React.FC = () => {
           handlePress={() => console.log('user profile')}
           text="User Profile"
         />
-        <UserProfileName />
+        <UserProfileName
+          handleClickAvatar={() => editProfileBottomSheetRef.current?.expand()}
+        />
         <UserBio />
         <MemoryGroup />
       </View>
+      <ProfileBottomSheet
+        editProfileBottomSheetRef={editProfileBottomSheetRef}
+      />
     </SafeAreaView>
   )
 }

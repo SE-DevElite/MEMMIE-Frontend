84eeb13d9561206ee6ebddbf045586da6cc90130
nutthanon <nutthanon.tho@gmail.com>
2024-03-13 23:03:26 +0700

    change: streak fire icon

--- src/components/friendProfile/Streak.tsx
+++ src/components/friendProfile/Streak.tsx
@@ -1,3 +1,4 @@
+import StreakFireIcon from '@/assets/svg/StreakFire'
 import { themes } from '@/common/themes/themes'
 import React from 'react'
 import { View, StyleSheet, Text, Image } from 'react-native'
@@ -6,20 +7,15 @@ const Streak: React.FC = () => {
   return (
     <>
       <View style={styles.container}>
-        <View>
-          <Text
-            style={{
-              fontSize: 18,
-              fontFamily: themes.fonts.medium,
-              color: themes.light.primary.hex
-            }}>
-            10
-          </Text>
-        </View>
-        <Image
-          source={require('@/assets/icons/streak.png')}
-          style={{ width: 13, height: 13 }}
-        />
+        <Text
+          style={{
+            fontSize: 18,
+            fontFamily: themes.fonts.medium,
+            color: themes.light.primary.hex
+          }}>
+          10
+        </Text>
+        <StreakFireIcon width={13} />
       </View>
     </>
   )
@@ -31,6 +27,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    gap: 2
   }
 })
